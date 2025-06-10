2f8fae32ee179273283812893c932a827c1e25ac
DindaAprilP <ndaprilianip@gmail.com>
2025-06-10 20:03:21 +0700

    tambah halaman login, register, text field

--- lib/awal.dart
+++ lib/awal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'login.dart';
 
 class splashAwal extends StatefulWidget {
   const splashAwal({super.key});
@@ -9,32 +10,42 @@ class splashAwal extends StatefulWidget {
 }
 
 class _splashAwalState extends State<splashAwal> {
+  @override
   void initState(){
     super.initState();
     Future.delayed(Duration(seconds: 3), (){
       Get.off (()=> LoginScreen());
     });
   }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/splashAwal.png",
-            width: 170,
-            height: 170,
-          ),
-          SizedBox(height: 5),
-          Container(
-            width: 100,
-            height: 150,
-            child: Image.asset("assets/IngetinHitam.png")
-          )
-        ],
+      body: 
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              child: Image.asset(
+                "assets/splashAwal.png",
+                width: 120,
+                height: 120,
+                ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              width: 100,
+              height: 150,
+              child: Image.asset(
+                "assets/ingetinHitam.png"
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ingetin/awal.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
